ee9a31d7d5a1a14896061ece4829b7a0ccab97dd
macaylak <113462288+macaylak@users.noreply.github.com>
2023-11-25 00:14:06 -0700

    seats and ticket tables

--- flightdb.sql
+++ flightdb.sql
@@ -2,6 +2,10 @@ DROP DATABASE IF EXISTS FLIGHTDB;
 CREATE DATABASE FLIGHTDB; 
 USE FLIGHTDB;
 
+-- airline in flight is not properly implemented yet***
+-- will update data better later***
+-- flight.crewList is being annoying***
+
 CREATE TABLE FLIGHT (
     flightNumber VARCHAR(5) NOT NULL,
     airline VARCHAR(20) NOT NULL,
@@ -47,12 +51,31 @@ CREATE TABLE CREW (
     PRIMARY KEY (crewID)
 );
 
+CREATE TABLE TICKET (
+    ticketNumber VARCHAR(10) NOT NULL,
+    flightNumber VARCHAR(5) NOT NULL,
+    passenger_fName VARCHAR(50) NOT NULL,
+	passenger_lName VARCHAR(50) NOT NULL,
+    seatNumber VARCHAR(3) NOT NULL,
+    class VARCHAR(10) NOT NULL,
+    userID INT NOT NULL,
+    PRIMARY KEY (ticketNumber),
+    FOREIGN KEY (flightNumber) REFERENCES FLIGHT(flightNumber),
+    FOREIGN KEY (userID) REFERENCES USERS(userID),
+    CONSTRAINT seatNumber CHECK (seatNumber BETWEEN '1A' AND '30C')
+
+);
+
+
 -- Inserting data into USERS table
 INSERT INTO USERS (isRegistered, firstName, lastName, street, city, country, email, password)
 VALUES
 (1, 'John', 'Smith', '123 Main St', 'New York', 'USA', 'email@email.com', '12345'),
 (1, 'Jane', 'Doe', '456 Main St', 'New York', 'USA', 'aaa@email.com', 'password'),
-(1, 'Your', 'Mom', '789 Main St', 'New York', 'USA', 'bbbb@email.com', '1111');
+(1, 'Your', 'Mom', '789 Main St', 'New York', 'USA', 'bbbb@email.com', '1111'),
+(0, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(0, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(0, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 -- Inserting data into ADDRESS table
 INSERT INTO ADDRESS (street, city, country, userID)
@@ -81,6 +104,30 @@ VALUES
 ('Frodo Baggins', 'Samwise Gamgee', 'Gandalf', 'Aragorn', 'Legolas', 'Gimli'),
 ('Tony Stark', 'Steve Rogers', 'Natasha Romanoff', 'Bruce Banner', 'Clint Barton', 'Thor');
 
+-- Inserting TICKET data
+INSERT INTO TICKET (ticketNumber, flightNumber, passenger_fName, passenger_lName, seatNumber, class, userID)
+VALUES
+('T001', 'FL001', 'John', 'Smith', '1A', 'First', 1),
+('T002', 'FL001', 'Jane', 'Doe', '1B', 'First', 2),
+('T003', 'FL001', 'Your', 'Mom', '1C', 'First', 3),
+('T004', 'FL002', 'John', 'Smith', '1A', 'First', 1),
+('T005', 'FL002', 'Jane', 'Doe', '1B', 'First', 2),
+('T006', 'FL002', 'Your', 'Mom', '1C', 'First', 3),
+('T007', 'FL003', 'John', 'Smith', '1A', 'First', 1),
+('T008', 'FL003', 'Jane', 'Doe', '1B', 'First', 2),
+('T009', 'FL003', 'Your', 'Mom', '1C', 'First', 3),
+('T010', 'FL004', 'John', 'Smith', '1A', 'First', 1),
+('T011', 'FL004', 'Jane', 'Doe', '1B', 'First', 2),
+('T012', 'FL004', 'Your', 'Mom', '1C', 'First', 3),
+('T013', 'FL005', 'John', 'Smith', '1A', 'First', 1),
+('T014', 'FL005', 'Jane', 'Doe', '1B', 'First', 2),
+('T015', 'FL005', 'Your', 'Mom', '1C', 'First', 3),
+('T016', 'FL006', 'John', 'Smith', '1A', 'First', 1),
+('T017', 'FL006', 'Jane', 'Doe', '1B', 'First', 2),
+('T018', 'FL006', 'Your', 'Mom', '1C', 'First', 3);
+
+
+
 ALTER TABLE FLIGHT
 ADD COLUMN crewID INT,
 ADD FOREIGN KEY (crewID) REFERENCES CREW(crewID);
@@ -88,9 +135,11 @@ ADD FOREIGN KEY (crewID) REFERENCES CREW(crewID);
 UPDATE FLIGHT SET crewID = 1 WHERE flightNumber = 'FL001';
 -- Assign other crew members to corresponding flights...
 
-SELECT f.flightNumber, f.airline, c.pilot, c.copilot, c.flightAttendant1, c.flightAttendant2, c.flightAttendant3, c.flightAttendant4
-FROM FLIGHT f
-JOIN CREW c ON f.crewID = c.crewID;
+-- displays properly rn
+-- SELECT f.flightNumber, f.airline, c.pilot, c.copilot, c.flightAttendant1, c.flightAttendant2, c.flightAttendant3, c.flightAttendant4
+-- FROM FLIGHT f
+-- JOIN CREW c ON f.crewID = c.crewID;
 
 -- SELECT * FROM FLIGHT;
 
+SELECT * FROM TICKET;
