3ee74d9b32f84ec7e75c955d4cce731f5f9bfe9a
123LeoMajesty <101436231+123LeoMajesty@users.noreply.github.com>
2023-11-24 19:54:43 -0700

    added main connector and user class
    sql statemnts need to be executed in ur db first

--- user.java
+++ user.java
@@ -1,4 +1,67 @@
+import java.util.Date;
 
-public class user{
-    
+public class User {
+    private int userID;
+    private String name;
+    private String email;
+    private Date dateOfBirth;
+    private String phoneNumber;
+    private String address;
+    //constructor
+    public User(int userID,String name,String email,Date dateOfBirth,String phoneNumber,String address){
+        this.userID = userID;
+        this.name = name;
+        this.email = email;
+        this.dateOfBirth = dateOfBirth;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+    // Getters and setters
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
