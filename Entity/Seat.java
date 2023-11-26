1ed8dedc21c7db9cd3ae19f568cbc5efae0ece50
Saba <sabayara0@gmail.com>
2023-11-26 02:46:47 -0700

    seat class added

---
+++ Entity/Seat.java
@@ -0,0 +1,53 @@
+package Entity;
+
+public class Seat {
+    private String seatNumber;
+    private String seatClass;
+    private boolean isBooked;
+    private double price;
+
+    // Constructor
+    public Seat(String seatNumber, String seatClass, double price) {
+        this.seatNumber = seatNumber;
+        this.seatClass = seatClass;
+        this.isBooked = false; // Initialize as not booked
+        this.price = price;
+    }
+
+    // Getters and Setters
+    public String getSeatNumber() {
+        return seatNumber;
+    }
+
+    public void setSeatNumber(String seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+    public String getSeatClass() {
+        return seatClass;
+    }
+
+    public void setSeatClass(String seatClass) {
+        this.seatClass = seatClass;
+    }
+
+    public boolean isBooked() {
+        return isBooked;
+    }
+
+    public void bookSeat() {
+        isBooked = true;
+    }
+
+    public void unbookSeat() {
+        isBooked = false;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
