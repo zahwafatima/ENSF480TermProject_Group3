c10469d467c9a32929d2fca3900506de13c940a4
123LeoMajesty <150176262+Shubh14042002@users.noreply.github.com>
2023-11-30 14:04:40 -0700

    Update FlightReservation.java

--- FlightReservation.java
+++ FlightReservation.java
@@ -1,15 +1,60 @@
+import Controller.DatabaseConnection;
+import Controller.UserController;
+
+import javax.swing.*;
+import Boundary.AirlineReservationSystem;
+import java.sql.*;
+
+public class FlightReservation {
+    public static void main(String[] args) {
+        // Step 1: Create a DatabaseConnection instance
+        DatabaseConnection db = DatabaseConnection.getOnlyInstance();
+        Connection connection = db.getConnection();
+        UserController usc = new UserController(connection);
+        if (connection != null) {
+            System.out.println("Connection to the database is successful!");
+            SwingUtilities.invokeLater(() -> {
+                AirlineReservationSystem frame = new AirlineReservationSystem(usc);
+                frame.setVisible(true);
+            });
+        } else {
+            System.out.println("Failed to connect to the database. Check your connection details.");
+        }
+    }
+}
 
 
-// public class FlightReservation{
+// import controller.DatabaseConnection;
+// import Controller.UserController;
+// import Entity.Address;
+// import Entity.User;
+// import Entity.Name;
+
+// public class FlightReservation {
 //     public static void main(String[] args) {
-//         DatabaseConnection dbConnection = new DatabaseConnection();
+//         // Step 1: Create a DatabaseConnection instance
+//         //EVERYONE: use the following like to send db to the controller classes
+//         DatabaseConnection db = DatabaseConnection.getOnlyInstance();
 
-//         if (dbConnection.dbConnect != null) {
+//         // Check if the database connection is successful
+//         if (dbConnection.getConnection() != null) {
 //             System.out.println("Connection to the database is successful!");
-//             SwingUtilities.invokeLater(() -> {
-//                 AirlineReservationSystem frame = new AirlineReservationSystem();
-//                 frame.setVisible(true);
-//             });
+
+//             // Step 2: Create a UserController instance using the DatabaseConnection
+//             UserController userController = new UserController(dbConnection);
+
+//             // Step 3: Create a sample User object
+//             Name sampleName = new Name("John", "Doe"); // Replace with actual names
+//             Address sampleAddress = new Address("123 Main St", "Cityville", "Countryland"); // Replace with actual address
+//             int samplePhoneNumber = 1234567890; // Replace with actual phone number
+//             String sampleEmail = "john.doe@example.com"; // Replace with actual email
+//             String samplePassword = "password123"; // Replace with actual password
+//             String sampleAccessLevel = "Customer"; // Replace with actual access level
+
+//             User sampleUser = new User(true, sampleName, sampleAddress, samplePhoneNumber, sampleEmail, samplePassword, sampleAccessLevel);
+
+//             // Call the addUserToDB method
+//             userController.addUserToDB(sampleUser);
 //         } else {
 //             System.out.println("Failed to connect to the database. Check your connection details.");
 //         }
@@ -17,35 +62,4 @@
 // }
 
 
-
-import Controller.DatabaseConnection;
-import Controller.DatabaseConnection;
-import javax.swing.*;
-//import Boundary.AirlineReservationSystem;
-
-import java.sql.Connection;
-import java.util.Map;
-
-import Controller.AdminController;
-//import Controller.UserController;
-import Entity.Address;
-import Entity.User;
-import Entity.Name;
-import Entity.Flight;
-import Entity.Aircraft;
-
-public class FlightReservation {
-    public static void main(String[] args) {
-        // Step 1: Create a DatabaseConnection instance
-        DatabaseConnection db = DatabaseConnection.getOnlyInstance();
-        Connection connection = db.getConnection();
-
-        // Check if the database connection is successful
-        if (connection != null) {
-
-            // add tests here 
-        } else {
-            System.out.println("Failed to connect to the database. Check your connection details.");
-        }
-    }
-}
+ 
